e85b20c284027b68e77170ccb66ff356929bae20
hoheesu <hoheesu@naver.com>
2022-09-28 14:49:39 +0900

    feat:

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-aloert('Your computer has been hacked. SEnd me 1.0btc to this addr, or Your COMPUTER will be die soon');
+alert('Your computer has been hacked. SEnd me 1.0btc to this addr, or Your COMPUTER will be die soon');
